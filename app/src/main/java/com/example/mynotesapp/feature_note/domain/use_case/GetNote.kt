a5e889d658b25810a8a87afaf5cff38a5cbfec9f
Djwerter Richmond Tetteh <Astrixrichmond@gmail.com>
2023-01-26 20:02:40 +0000

    Completed Note Detail UI

---
+++ app/src/main/java/com/example/mynotesapp/feature_note/domain/use_case/GetNote.kt
@@ -0,0 +1,10 @@
+package com.example.mynotesapp.feature_note.domain.use_case
+
+import com.example.mynotesapp.feature_note.domain.module.Note
+import com.example.mynotesapp.feature_note.domain.repository.NoteRepository
+
+class GetNote(private val repository: NoteRepository) {
+    suspend operator fun invoke(id: Int): Note? {
+        return repository.getNoteById(id)
+    }
+}
